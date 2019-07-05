de3ecab7d40b9ecd6fbf7e89f717dadd648b9f28
melonybox <grandmadcow@gmail.com>
2019-07-05 12:13:50 -0400

    Auto Save updates user chapter, doesnt double save same chapter.

--- app/controllers/api/v1/histories_controller.rb
+++ app/controllers/api/v1/histories_controller.rb
@@ -6,12 +6,23 @@ class Api::V1::HistoriesController < ApplicationController
 
   def create
     chapter = History.new(user_id: params[:user_id], madlib_id: params[:madlib_id], placeHolderFilled: params[:placeHolderFilled])
+    exist = History.find_by(user_id: params[:user_id], madlib_id: params[:madlib_id])
+    user = User.find(params[:user_id])
 
-    if chapter.save
-			render json: chapter
-		else
-			render json: {errors: chapter.errors.full_messages}
-		end
+    if !!exist
+      render json: {errors: "Chapter is Already Saved."}
+    else
+      if chapter.save
+        if !!user[chapter]
+          user.update(username: user[:username], password_digest: user[:password_digest], current_chapter: 1)
+        else
+          user.update(username: user[:username], password_digest: user[:password_digest], current_chapter: (user[:current_chapter].to_i + 1))
+        end
+  			render json: chapter
+  		else
+  			render json: {errors: chapter.errors.full_messages}
+  		end
+    end
 	end
 
   def update
