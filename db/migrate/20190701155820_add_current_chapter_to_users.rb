58a6717939bb89b56c1ee30b9cc7080efda2637a
melonybox <grandmadcow@gmail.com>
2019-07-05 17:35:15 -0400

    add default value tp currentchapter

--- db/migrate/20190701155820_add_current_chapter_to_users.rb
+++ db/migrate/20190701155820_add_current_chapter_to_users.rb
@@ -1,5 +1,5 @@
 class AddCurrentChapterToUsers < ActiveRecord::Migration[5.2]
   def change
-    add_column :users, :current_chapter, :string
+    add_column :users, :current_chapter, :string, :default => 0
   end
 end
